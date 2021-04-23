20a6f2cf18a100af4fa707d562a08977f884170f
Vishal Dukare <Vishal14583@Mastek.Com>
2021-04-23 16:07:28 +0530

    Add project files.

---
+++ AzureTokenWindowsAuth/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace AzureTokenWindowsAuth
+{
+    public class Configuration
+    {
+        protected IConfigurationRoot ConfigurationRoot;
+        public string MicrosoftOnlineLoginUrl { get; set; }
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public bool IsRunningOnLocalMachine { get; set; }
+
+        public Configuration()
+        {
+            ConfigurationRoot = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json", false)
+                                .Build();
+
+            MicrosoftOnlineLoginUrl = ConfigurationRoot.GetSection("MicrosoftOnlineLoginUrl").Value;
+            TenantId = ConfigurationRoot.GetSection("TenantId").Value;
+            ClientId = ConfigurationRoot.GetSection("ClientId").Value;
+            ClientSecret = ConfigurationRoot.GetSection("ClientSecret").Value;
+            IsRunningOnLocalMachine = bool.Parse(ConfigurationRoot.GetSection("IsRunningOnLocalMachine").Value);
+        }
+    }
+}
---
+++ AzureTokenWindowsAuth/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureTokenWindowsAuth
+{
+    public static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string jwtToken;
+            Configuration configuration = new Configuration();
+
+            AuthTokenProvider authTokenProvider = new AuthTokenProvider();
+            jwtToken = await authTokenProvider.GenerateToken(configuration.ClientId, configuration.TenantId, configuration.MicrosoftOnlineLoginUrl);
+
+            Console.WriteLine(jwtToken);
+        }
+    }
+
+    public class AuthTokenProvider
+    {
+        public async Task<string> GenerateToken(string clientId, string tenantId, string microsoftOnlineLoginUrl)
+        {
+            string authority = $"{microsoftOnlineLoginUrl}{tenantId}";
+            string[] scopes = new string[] { $"{clientId}/.default" };
+
+            IPublicClientApplication debugapp = PublicClientApplicationBuilder
+                                                  .Create(clientId)
+                                                  .WithAuthority(authority)
+                                                  .Build();
+
+            //Acquiring token through windows auth
+            AuthenticationResult tokenTask = await debugapp.AcquireTokenByIntegratedWindowsAuth(scopes)
+                                                           .ExecuteAsync();
+            return tokenTask.AccessToken;
+        }
+    }
+}
